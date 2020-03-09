a77e44ff4e74937842505e0acdd81e8474fd154e
Nick Shevchenko <shevchenkokolya@mail.ru>
2020-03-09 18:16:56 +0300

    from-desktop

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import bridge from '@vkontakte/vk-bridge';
+import App from './App';
+// import registerServiceWorker from './sw';
+
+// Init VK  Mini App
+bridge.send('VKWebAppInit');
+
+// Если вы хотите, чтобы ваше веб-приложение работало в оффлайне и загружалось быстрее,
+// расскомментируйте строку с registerServiceWorker();
+// Но не забывайте, что на данный момент у технологии есть достаточно подводных камней
+// Подробнее про сервис воркеры можно почитать тут — https://vk.cc/8MHpmT
+// registerServiceWorker();
+
+ReactDOM.render(<App />, document.getElementById('root'));
